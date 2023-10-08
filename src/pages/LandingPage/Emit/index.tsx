f2d08deed6df81e70a9009443ce49990c3632355
senicko <25861704+senicko@users.noreply.github.com>
2023-10-08 07:03:32 +0200

    Refactor data page styles

--- src/pages/LandingPage/Emit/index.tsx
+++ src/pages/LandingPage/Emit/index.tsx
@@ -1,29 +1,38 @@
 import styles from "./Emit.module.css";
 import clsx from "clsx";
 
+import EarthImage from "./assets/earth.png";
+import OrbitImage from "./assets/orbit.png";
+import IssImage from "./assets/iss.webp";
+
 export const Emit = () => {
   return (
-    <div>
-      <div className={styles.earth}></div>
-      <div className={clsx(styles.iss, styles.floating)}></div>
-      <div className={clsx(styles.orbit)}></div>
-      <div className={clsx(styles.orbit)}></div>
-      <div className={styles.sideContainer}>
-        <div className={clsx("heading1")}>EMIT</div>
-        <div className={clsx(styles.heading1smaller)}>
-          The Earth Surface Mineral
-          <br /> Dust Source Investigation
-        </div>
-        <div className={styles.description}>
-          EMIT, NASA's advanced sensor, launched in July 2022, is aboard the
-          International Space Station. It boasts a hyperspectral camera with
-          60x60 m spatial resolution, collecting valuable data for various
-          applications, from crop monitoring to methane detection.
-        </div>
-        <div className={styles.description}>
-          Its primary mission, though, is understanding how surface dust affects
-          global and local climate change. EMIT's data is openly accessible for
-          further research."
+    <div className={styles.container}>
+      <div className={styles.images_container}>
+        <img src={OrbitImage} className={clsx(styles.orbit)} />
+        <img src={EarthImage} className={styles.earth} />
+        <img src={IssImage} className={clsx(styles.iss, styles.floating)} />
+      </div>
+
+      <div className={styles.layout}>
+        <div className={styles.placeholder}></div>
+        <div className={styles.content_container}>
+          <div className="heading1">data</div>
+          <div className="heading4">
+            The Earth Surface Mineral
+            <br /> Dust Source Investigation
+          </div>
+          <div className={styles.description}>
+            EMIT, NASA's advanced sensor, launched in July 2022, is aboard the
+            International Space Station. It boasts a hyperspectral camera with
+            60x60 m spatial resolution, collecting valuable data for various
+            applications, from crop monitoring to methane detection.
+          </div>
+          <div className={styles.description}>
+            Its primary mission, though, is understanding how surface dust
+            affects global and local climate change. EMIT's data is openly
+            accessible for further research."
+          </div>
         </div>
       </div>
     </div>
