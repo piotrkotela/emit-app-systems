446ebb1e5e397e8e402f30a11bea049105bc4bbe
piotrkotela <60201080+piotrkotela@users.noreply.github.com>
2023-10-08 11:53:39 +0200

    Map final version (#26)

--- src/components/Map/MapChart.tsx
+++ src/components/Map/MapChart.tsx
@@ -3,23 +3,34 @@ import geoData from "./resources/geo.json";
 import properties from "./resources/properties.json";
 import { scaleLinear } from "d3-scale";
 
-const MapChart = () => {
+
+const MapChart = ({setImeiContent}: any) => {
   //@ts-ignore
-  const colorScale = scaleLinear().domain([0, 51]).range(["#FFF", "#06F"]);
+  const colorScale = scaleLinear().domain([0, 51]).range(["#2E2E2E", "#3705E4"]);
   const showPopUpHandler = (x: any) => {
     console.log(x);
   };
   return (
-        <ComposableMap>
+        <ComposableMap width={800} height={500}>
           <Geographies geography={geoData}>
             {({ geographies }) =>
               geographies.map((geo) => (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
+                  onMouseEnter={() => {
+                    setImeiContent({country_name: geo.properties.name, imei_idx: properties[geo.id as keyof typeof properties] ?? 0});
+                  }}
+                  onMouseLeave={() => {
+                    setImeiContent("");
+                  }}
                   // @ts-ignore
                   fill={colorScale(properties[geo.id as keyof typeof properties] ?? 0)}
-                  onMouseOver={() => showPopUpHandler(geo.id)}
+                  style={{
+                    hover: {
+                      fill: "white",
+                    }
+                  }}
                 />
               ))
             }
---
+++ src/pages/LandingPage/map/MapLegend.tsx
@@ -0,0 +1,15 @@
+import styles from "./MapLegend.module.css";
+
+const MapLegend = () => {
+  return (
+    <div>
+      <div className={styles.legend}>
+        <span>0</span>
+        <span>26</span>
+      </div>
+      <div className={styles.legendBar} />
+      <span className={styles.imei}>IMEI INDEX</span>
+    </div>
+  );
+};
+export default MapLegend;
