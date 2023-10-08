f254dcd7c2ab3e670d9bb4db2aa5704bbca76b7b
senicko <25861704+senicko@users.noreply.github.com>
2023-10-08 06:53:51 +0200

    Refactor data page styles

--- src/pages/LandingPage/Data/index.tsx
+++ src/pages/LandingPage/Data/index.tsx
@@ -1,25 +1,30 @@
 import styles from "./Data.module.css";
-import clsx from "clsx";
+
+import IssImage from "./assets/iss.webp";
+import BgImage from "./assets/5.png";
 
 export const Data = () => {
   return (
-    <div>
-      <div className={styles.rain}></div>
-      <div className={styles.bg}></div>
-      <div className={clsx(styles.iss_large)}></div>
-      <div className={styles.sideContainer}>
-        <div className={clsx("heading1")}>data</div>
-        <div className={clsx(styles.subtitle)}>How we can see the unseen?</div>
-        <div className={styles.description}>
-          The data collected by EMIT provides a lot of possible applications
-          from crop condition monitoring to precise methane leak detection.
-          However, it's main mission is to determine the impact of surface dust
-          migration on climate change issues either globally or locally.
-        </div>
-        <div className={styles.description}>
-          Measurements captured by EMIT are openly available to the full science
-          and applications communities for the range of additional
-          investigations they enable.
+    <div className={styles.container}>
+      <img src={BgImage} className={styles.bg} />
+      <img src={IssImage} className={styles.iss_large} />
+
+      <div className={styles.layout}>
+        <div className={styles.placeholder}></div>
+        <div className={styles.content_container}>
+          <div className="heading1">data</div>
+          <div className="heading4">How we can see the unseen?</div>
+          <div className={styles.description}>
+            The data collected by EMIT provides a lot of possible applications
+            from crop condition monitoring to precise methane leak detection.
+            However, it's main mission is to determine the impact of surface
+            dust migration on climate change issues either globally or locally.
+          </div>
+          <div className={styles.description}>
+            Measurements captured by EMIT are openly available to the full
+            science and applications communities for the range of additional
+            investigations they enable.
+          </div>
         </div>
       </div>
     </div>
